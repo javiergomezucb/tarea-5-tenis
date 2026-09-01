069988a33a1b5b4d1d971306da4fc7ed0da2618c
javichosGIT <javier.gomez@ucb.edu.bo>
2026-09-01 10:25:04 -0400

    feat: jugador 1 anota 1 vez muestra 15 - Love

--- src/tenis.js
+++ src/tenis.js
@@ -1,5 +1,19 @@
+// src/tenis.js
 class Tennis {
+  constructor() {
+    this.puntosJ1 = 0;
+    this.puntosJ2 = 0;
+  }
+
+  jugador1Anota() {
+    this.puntosJ1++;
+  }
+
+
   obtenerScore() {
+    if (this.puntosJ1 === 1) {
+      return "15 - Love";
+    }
     return "Love - Love";
   }
 }
--- src/tenis.test.js
+++ src/tenis.test.js
@@ -5,4 +5,11 @@ describe("Tennis", () => {
     let tennis = new Tennis();
     expect(tennis.obtenerScore()).toEqual("Love - Love");
   });
+
+ it("deberia mostrar 15 - Love cuando el jugador 1 anota una vez", () => {
+    let tennis = new Tennis();
+    tennis.jugador1Anota();
+    expect(tennis.obtenerScore()).toEqual("15 - Love");
+  });
+
 });
