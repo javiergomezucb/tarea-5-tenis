16dabdc8b48a4b9767c109293d65efbf2eded79d
javichosGIT <javier.gomez@ucb.edu.bo>
2026-09-03 08:28:30 -0400

    feat: interfaz grafica conectada con la clase Tennis final

--- src/tenis.js
+++ src/tenis.js
@@ -3,6 +3,7 @@ class Tennis {
   constructor() {
     this.puntosJ1 = 0;
     this.puntosJ2 = 0;
+    this.nombresPuntos = ["Love", "15", "30", "40"];
   }
 
   jugador1Anota() {
@@ -14,54 +15,31 @@ class Tennis {
   }
 
   obtenerScore() {
-    if (this.puntosJ1 === 5 && this.puntosJ2 === 3) {
+    // 1. Ganador del game (al menos 4 puntos y diferencia de 2)
+    if (this.puntosJ1 >= 4 && this.puntosJ1 - this.puntosJ2 >= 2) {
       return "Game for Player 1";
     }
-
-    if (this.puntosJ1 === 3 && this.puntosJ2 === 3) {
-      return "Deuce";
-    }
-
-    if (this.puntosJ1 === 4 && this.puntosJ2 === 3) {
-      return "Advantage for Player 1";
-    }
-
-    if (this.puntosJ1 === 3 && this.puntosJ2 === 4) {
-      return "Advantage for Player 2";
-    }
-
-    if (this.puntosJ1 === 4) {
-      return "Game for Player 1";
-    }
-    if (this.puntosJ2 === 4) {
+    if (this.puntosJ2 >= 4 && this.puntosJ2 - this.puntosJ1 >= 2) {
       return "Game for Player 2";
     }
 
-    if (this.puntosJ1 === 2 && this.puntosJ2 === 1) {
-      return "30 - 15";
+    // 2. Empate en 40 o superior (Deuce)
+    if (this.puntosJ1 >= 3 && this.puntosJ1 === this.puntosJ2) {
+      return "Deuce";
     }
 
-    if (this.puntosJ1 === 3) {
-      return "40 - Love";
-    }
-    if (this.puntosJ1 === 2) {
-      return "30 - Love";
-    }
-    if (this.puntosJ1 === 1) {
-      return "15 - Love";
-    }
-
-    if (this.puntosJ2 === 3) {
-      return "Love - 40";
-    }
-    if (this.puntosJ2 === 2) {
-      return "Love - 30";
-    }
-    if (this.puntosJ2 === 1) {
-      return "Love - 15";
+    // 3. Ventajas tras deuce
+    if (this.puntosJ1 >= 3 && this.puntosJ2 >= 3) {
+      if (this.puntosJ1 - this.puntosJ2 === 1) {
+        return "Advantage for Player 1";
+      }
+      if (this.puntosJ2 - this.puntosJ1 === 1) {
+        return "Advantage for Player 2";
+      }
     }
 
-    return "Love - Love";
+    // 4. Marcadores regulares combinados
+    return this.nombresPuntos[this.puntosJ1] + " - " + this.nombresPuntos[this.puntosJ2];
   }
 }
 
