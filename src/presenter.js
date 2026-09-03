3031a436bb25041b9366f8f72793b022e4c45eeb
javichosGIT <javier.gomez@ucb.edu.bo>
2026-09-03 07:44:13 -0400

    feat: interfaz grafica conectada con la clase Tennis

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,27 @@
-import sumar from "./sumador";
+import Tennis from "./tenis.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+let juego = new Tennis();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const marcadorDiv = document.querySelector("#marcador");
+const btnJugador1 = document.querySelector("#btn-jugador1");
+const btnJugador2 = document.querySelector("#btn-jugador2");
+const btnReiniciar = document.querySelector("#btn-reiniciar");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+function refrescarPantalla() {
+  marcadorDiv.textContent = juego.obtenerScore();
+}
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+btnJugador1.addEventListener("click", () => {
+  juego.jugador1Anota();
+  refrescarPantalla();
+});
+
+btnJugador2.addEventListener("click", () => {
+  juego.jugador2Anota();
+  refrescarPantalla();
+});
+
+btnReiniciar.addEventListener("click", () => {
+  juego = new Tennis();
+  refrescarPantalla();
 });
